e097bc75581d0b4a58fcfdfe883ce3ea0d554b95
wakitv <77536250+wakitv@users.noreply.github.com>
2026-03-03 16:47:41 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='wackybuds-cfr-v3.8.1';
+const CACHE='wackybuds-cfr-v3.8.3';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html'])).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
