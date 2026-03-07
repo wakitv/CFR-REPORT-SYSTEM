2b620ef66925ba691039417265d000f6a125ccdb
wakitv <77536250+wakitv@users.noreply.github.com>
2026-03-07 20:11:28 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='wackybuds-cfr-v5.6.2';
+const CACHE='wackybuds-cfr-v5.8.0';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json'])).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.hostname.includes('script.google.com'))return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{if(r.ok){caches.open(CACHE).then(ca=>ca.put(e.request,r.clone()));}return r;}).catch(()=>caches.match('./index.html')));});
