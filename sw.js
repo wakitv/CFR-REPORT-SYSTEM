0f84d47ab5433cee85926d8712ca50d3bab5715a
wakitv <77536250+wakitv@users.noreply.github.com>
2026-03-08 12:40:31 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -24,18 +24,16 @@ self.addEventListener('fetch', e => {
   if (url.hostname.includes('script.google.com')) return;
   
   e.respondWith(
-    caches.match(e.request)
-      .then(cached => {
-        if (cached) return cached;
-        return fetch(e.request)
-          .then(res => {
-            if (res.ok) {
-              const clone = res.clone();
-              caches.open(CACHE).then(c => c.put(e.request, clone));
-            }
-            return res;
-          })
-          .catch(() => caches.match('./index.html'));
-      })
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      
+      return fetch(e.request).then(res => {
+        if (res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => caches.match('./index.html'));
+    })
   );
 });
